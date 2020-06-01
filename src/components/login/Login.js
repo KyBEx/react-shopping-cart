09f1aaed4861e2b53a432477aaf8b5a4d6969a8f
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2020-06-01 12:02:29 -0600

    added error modal

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,19 @@
 import React, {useState} from "react";
 import LoginForm from "../forms/LoginForm";
 import logo from "../../assets/padlock.png"
+import Error from "../common/error/Error";
 export default function Login() {
     const [submitting, setSubmitting] = useState(false);
     const [credentialError, setCredentialError] = useState(false);
+    const {setOpen, setMessage, modal} = Error();
 
     function onSubmit() {
-
+        setMessage('There has been an error, as submit isnt connected');
+        setOpen(true);
     }
     return (
         <div className="App-background-container">
+            {modal}
             <div className="App-form-container">
                 <img src={logo} className="App-image-size-big App-image-position" />
                 <LoginForm
