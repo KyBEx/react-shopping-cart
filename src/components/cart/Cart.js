c0d356ac947203cddf66789d7604333dce76870b
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2020-06-08 10:24:38 -0600

    added basic styling to Cart component

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "semantic-ui-react";
+import "./Cart.css";
 
 export default function Cart() {
-  const [items, setItems] = useState({});
+  const [items, setItems] = useState([]);
   useEffect(() => {
     fetch("./mockData.json")
       .then((response) => response.json())
@@ -18,5 +20,23 @@ export default function Cart() {
         setItems(Object.values(itemHash));
       });
   }, []);
-  return <></>;
+  return (
+    <div>
+      {items.map((item) => (
+        <div className="cart-item-display">
+          <div className="cart-image-display">
+            <img src={item.image} />
+          </div>
+          <div className="cart-info-display">
+            {item.brand} {item.model} {item.year}
+          </div>
+          <div className="cart-cost-display">{item.cost}</div>
+          <div className="cart-count-display">{item.count}</div>
+          <div>
+            <Button>REMOVE</Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
